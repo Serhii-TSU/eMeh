a60175f18ba5abbef89e496b56a7a41d36ee3128
S&D <sergiy.tsu@gmail.com>
2023-12-03 14:14:41 +0000

    slogan change

--- eMeh/ClientApp/src/components/NavMenu.js
+++ eMeh/ClientApp/src/components/NavMenu.js
@@ -25,7 +25,10 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">eMeh</NavbarBrand>
+          <NavbarBrand tag={Link} to="/" className="brand">
+            <div className="brand-name">eMeh</div>
+            <div className="slogan">When you need something, but you're not entirely sure what.</div>
+          </NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
